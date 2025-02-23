036ff6d21739de19e142bd757cf82ce79ef24f10
Ricardo Tortorelli <ricktorelli@MacBook-Pro-de-Ricardo.local>
2025-02-23 11:05:57 -0300

    autoload.php

--- app/public/autoload.php
+++ app/public/autoload.php
@@ -4,8 +4,6 @@ function checkDatabaseConnection($pdo) {
         $stmt = $pdo->query("SELECT 1");
         if ($stmt) {
             return "Conexão com o banco de dados está ok. Nome do banco de dados: " . $_ENV['DB_DATABASE'];
-        } else {
-            return "Erro ao verificar o banco de dados.";
         }
     } catch (PDOException $e) {
         return "Erro ao verificar o banco de dados: " . $e->getMessage();
